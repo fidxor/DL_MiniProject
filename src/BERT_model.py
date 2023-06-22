06e1826137b1cbcf9af77b3835ef6bbf1a07ca00
fidxor <fidxordl5404@gmail.com>
2023-06-22 09:50:28 +0900

    update 파일 정리
    공부하면서 test 했던 파일 따로 폴더로 이동
    실행소스 정리 후 src 폴더로 이동

---
+++ src/BERT_model.py
@@ -0,0 +1,68 @@
+import torch
+import numpy as np
+from torch import nn
+from transformers import BertModel
+from transformers import BertTokenizer
+
+__all__ = ['Dataset', 'BertClassifier']
+
+tokenizer = BertTokenizer.from_pretrained('bert-large-uncased')
+
+labels = {"INFJ" : 0, "INTJ" : 1, "INFP" : 2, "INTP" : 3, "ENFJ" : 4, "ENTJ" : 5, 
+              "ENFP" : 6, "ENTP" : 7, "ISFJ" : 8, "ISTJ" : 9, "ISFP" : 10, "ISTP" : 11,
+                "ESFJ" : 12, "ESTJ" : 13, "ESFP" : 14, "ESTP" : 15}
+
+# 결과 출력을 위해 labels key와 value 바꿔주기
+resultLabels = {v:k for k,v in labels.items()}
+
+class Dataset(torch.utils.data.Dataset):   
+    
+    def __init__(self, df):
+
+        self.labels = [labels[label] for label in df['type']]
+        self.texts = [tokenizer(text, 
+                               padding='max_length', max_length = 512, truncation=True,
+                                return_tensors="pt") for text in df['posts']]
+
+    def classes(self):
+        return self.labels
+
+    def __len__(self):
+        return len(self.labels)
+
+    def get_batch_labels(self, idx):
+        # Fetch a batch of labels
+        return np.array(self.labels[idx])
+
+    def get_batch_texts(self, idx):
+        # Fetch a batch of inputs
+        return self.texts[idx]
+
+    def __getitem__(self, idx):
+
+        batch_texts = self.get_batch_texts(idx)
+        batch_y = self.get_batch_labels(idx)
+
+        return batch_texts, batch_y
+    
+
+class BertClassifier(nn.Module):
+
+    def __init__(self, dropout=0.5):
+
+        super(BertClassifier, self).__init__()
+
+        self.bert = BertModel.from_pretrained('bert-large-uncased')
+        self.dropout = nn.Dropout(dropout)
+        self.linear = nn.Linear(1024, 16)
+        self.relu = nn.ReLU()
+
+    def forward(self, input_id, mask):
+
+        _, pooled_output = self.bert(input_ids= input_id, attention_mask=mask,return_dict=False)
+        dropout_output = self.dropout(pooled_output)
+        linear_output = self.linear(dropout_output)
+        final_layer = self.relu(linear_output)
+
+        return final_layer   
+        
